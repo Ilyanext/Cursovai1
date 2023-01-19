cc2ebe329bf8775aa9d4c54609943b07e2157eda
Ilya <rabota003@yandex.ru>
2023-01-19 21:20:28 +0300

    Fixced code

--- src/Cursovailite/Employee.java
+++ src/Cursovailite/Employee.java
@@ -4,16 +4,21 @@ public class Employee {
     private String fullName;
     private int department;
     private int salary;
-    static int numbersOfEmployee;
-    static int id;
+    static int numbersOfEmployee = 0;
+    private int id = 0;
 
     public Employee(String fullName, int department, int salary) {
         this.fullName = fullName;
         this.department = department;
         this.salary = salary;
-        numbersOfEmployee++;
-        id = +numbersOfEmployee;
+        id = numbersOfEmployee++;
     }
+
+//    public void EmployeeDontDepatment (String fullName, int salary, int id) {
+//        this.fullName = fullName;
+//        this.salary = salary;
+//        this.id = id;
+//    }
 
     public String getFullName() {
         return fullName;
@@ -46,8 +51,8 @@ public class Employee {
     @Override
     public String toString() {
         return "Сотрудник " +
-                "Ф.И.О.: " + fullName  +
+                "Ф.И.О.: " + fullName +
                 ", отдел: " + department +
-                ", зарплата: " + salary+ " руб.";
+                ", зарплата: " + salary + " руб.";
     }
 }
